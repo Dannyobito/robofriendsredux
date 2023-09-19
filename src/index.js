deed18a43d4e351c26e834da3478e5ae9ca70d9c
Dannyobito <dannyobito05@gmail.com>
2023-09-19 12:08:43 +0100

    created reducer for searchfield

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider, Connect } from 'react-redux';
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals';
